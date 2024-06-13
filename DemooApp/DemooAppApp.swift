2e2b9f673588ac64055b9a6343e06b0bc5b432ea
Dikook <vimvom931@gmail.com>
2024-06-13 10:29:54 +0500

    Initial Commit

---
+++ DemooApp/DemooAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemooAppApp.swift
+//  DemooApp
+//
+//  Created by Diar Orynbek on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemooAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
