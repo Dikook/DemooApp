1f6ec55587e812efd987514d6039d5206b38dee3
Dikook <vimvom931@gmail.com>
2024-06-13 10:35:24 +0500

    second commit to new branch version-1.0.0

---
+++ DemooApp/NewFile.swift
@@ -0,0 +1,10 @@
+//
+//  NewFile.swift
+//  DemooApp
+//
+//  Created by Diar Orynbek on 13.06.2024.
+//
+
+import Foundation
+
+// TOday is day when i started work project with GIT  I am Happiest man in the world!
